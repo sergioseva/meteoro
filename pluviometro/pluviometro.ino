bc9abaaa664fb551b7c1cc8704810108f113b5b9
sergioseva <sergioseva@gmail.com>
2016-07-01 15:32:09 -0300

    Rediseño y escritura en SD

--- pluviometro/pluviometro.ino
+++ pluviometro/pluviometro.ino
@@ -1,4 +1,4 @@
-
+#include <SD.h>
 #include <Wire.h>
 #include <LiquidCrystal.h>
 #include <RtcDS3231.h>
@@ -13,6 +13,8 @@ struct config_t
 
 
 RtcDS3231 Rtc;
+RtcDateTime now;
+RtcTemperature temp
 float medicion=0;
 String  unidades= "mm";
 int pin1 = 9;
@@ -23,7 +25,10 @@ LiquidCrystal lcd(8,9,4,5,6,7);
 
 void setup() {
   // seteo reloj
-    Serial.begin(9600);
+   Serial.begin(9600);
+   
+   
+   Wire.begin();
    EEPROM_readAnything(0, configuration);
    
 	  if (configuration.deflt==255) {
@@ -35,7 +40,21 @@ void setup() {
 	  }
 
     seteoReloj();
-
+   //SD CARD
+   Serial.print("Initializing SD card...");
+   // make sure that the default chip select pin is set to
+   // output, even if you don't use it:
+   pinMode(10, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+   if (!SD.begin(chipSelect)) {
+     Serial.println("Card failed, or not present");
+     // don't do anything more:
+     
+   } else
+   Serial.println("card initialized.");
+  
+	
     // Inicializamos la lectura del encoder
     pinMode(pin1, INPUT);
     pinMode(pin2, INPUT);
@@ -51,15 +70,20 @@ void setup() {
 }
 
 void loop() {
+	
+	String datestring;
   // muestro la hora y temperatura
     
     
      // if they push the "Save" button, save their configuration
     if (digitalRead(13) == HIGH)
         EEPROM_writeAnything(0, configuration);
-    mostrarHoraYTemp();
+    
+    
+    leerHoraYTemp();
     leerMedicion();
- 
+    mostrarDatos(true);  // Escribimos el valor en pantalla
+    
     delay(10000); // ten seconds
 
 }
@@ -67,6 +91,21 @@ void loop() {
 void resetValues(){
 medicion=0;  
 }
+
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void leerHoraYTemp(){
+   if (!Rtc.IsDateTimeValid()) 
+    {
+        // Common Cuases:
+        //    1) the battery on the device is low or even missing and the power line was disconnected
+        Serial.println("RTC lost confidence in the DateTime!");
+    }
+
+    now = Rtc.GetDateTime();   
+ }
+
 
 void leerMedicion(){
   // Lectura de A, si es 0, volvemos a medir para asegurar el valor
@@ -82,14 +121,24 @@ void leerMedicion(){
     int valor_b = digitalRead(pin2);
     if(valor_b == 1) medicion=medicion+0.2;  // Si B = 1, aumentamos el contador
     else medicion=medicion-0.2;  // Si B = 0, reducimos el contador
-    escribir(true);  // Escribimos el valor en pantalla
-  }
+   }
   anterior = actual;
 }
   
 
-void escribir(boolean serial)
+void mostrarDatos(boolean serial)
 {
+	
+  //muestro fecha y hora
+  datestring=formatDateTime(now);
+  if (serial)
+	    Serial.print(datestring);
+	    
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print(datestring);
+  //muestro la medicion     
+  
   String s = String(medicion);  // Convertimos el número en texto
   //s.toCharArray(texto, 10);  // Convertimos el texto en un formato compatible
   if (serial)
@@ -97,7 +146,45 @@ void escribir(boolean serial)
   lcd.setCursor(2,0);
   lcd.print(s + " " + unidades);
   
+  //escribo en la SD, aca hay que controlar que sea en el periodo fijado por el usuario
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(datestring + "," + s + " " + unidades);
+    dataFile.close();
+    
+  }  
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  } 
+  
+  
 }
+
+
+
+
+String formatDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            dt.Month(),
+            dt.Day(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+   
+    return datestring;
+}
+
+
+
 
 void seteoReloj() {
     Serial.print("compiled: ");
@@ -157,43 +244,3 @@ void seteoReloj() {
     Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone); 
   }
 
- #define countof(a) (sizeof(a) / sizeof(a[0]))
-
-void mostrarHoraYTemp(){
-   if (!Rtc.IsDateTimeValid()) 
-    {
-        // Common Cuases:
-        //    1) the battery on the device is low or even missing and the power line was disconnected
-        Serial.println("RTC lost confidence in the DateTime!");
-    }
-
-    RtcDateTime now = Rtc.GetDateTime();
-    printDateTime(now, true);
-    Serial.println();
-
-    RtcTemperature temp = Rtc.GetTemperature();
-    Serial.print(temp.AsFloat());
-    Serial.println("C");
-  }
-
-
-void printDateTime(const RtcDateTime& dt, boolean serial)
-{
-    char datestring[20];
-
-    snprintf_P(datestring, 
-            countof(datestring),
-            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
-            dt.Month(),
-            dt.Day(),
-            dt.Year(),
-            dt.Hour(),
-            dt.Minute(),
-            dt.Second() );
-    if (serial)
-    Serial.print(datestring);
-    
-    lcd.clear();
-    lcd.setCursor(1,0);
-    lcd.print(datestring);
-}
