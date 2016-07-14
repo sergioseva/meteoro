130c9aba38dfb5cf1383a59d1ef5262600ddb984
sergioseva <sergioseva@gmail.com>
2016-07-14 12:42:01 -0300

    Acumulado diario y mensual

--- pluviografo/pluviografo.ino
+++ pluviografo/pluviografo.ino
@@ -3,20 +3,20 @@
 #include <SD.h>
 #include <Wire.h>
 #include <RtcDS3231.h>
-#include <EEPROM.h>
 #include "EEPROMAnything.h"
+#include <math.h>
 
 
-#define Reset 0
-#define CambiarHora 1
-#define CambiarFecha 2
-#define Salir 2
-unsigned int setting = 0;
+
 
 struct config_t
 {   byte deflt; 
     float medicion;
-    int minutos_logueo;
+	int minutos_logueo;
+	float acumulado_mes;
+	float acumulado_dia;
+	int mes_actual;
+	int dia_actual;
 } configuration;
 
 
@@ -24,7 +24,6 @@ RtcDS3231 Rtc;
 RtcDateTime now;
 float medicion=0;
 String  unidades= "mm";
-
 LCDKeypad lcd;
 
 //LiquidCrystal lcd(8,9,4,5,6,7); 
@@ -82,54 +81,45 @@ void doEncoderB(){
 
 void setup() {
    
-   Serial.begin(9600);
+	Serial.begin(9600);
+	Wire.begin();
    
-   
-   Wire.begin();
-   int count=EEPROM_readAnything(0, configuration);
+	pinMode(encoderPinA, INPUT); 
+	pinMode(encoderPinB, INPUT); 
+	// turn on pullup resistors
+	digitalWrite(encoderPinA, LOW);
+	digitalWrite(encoderPinB, LOW);
+	// encoder pin on interrupt 0 (pin 2)
+	attachInterrupt(digitalPinToInterrupt(encoderPinA), doEncoderA, RISING);
+	// encoder pin on interrupt 1 (pin 3)
+	attachInterrupt(digitalPinToInterrupt(encoderPinB), doEncoderB, RISING);
+  
+  int count=EEPROM_readAnything(0, configuration);
    Serial.println(count);
    Serial.println(configuration.deflt);
    Serial.println(configuration.medicion);
    Serial.println(configuration.minutos_logueo);
    
 	  if (configuration.deflt==255) {
-		// es la primera vez que se inicia el sistema
-		  configuration.deflt=0;
-		  configuration.medicion=0;
-		  configuration.minutos_logueo=10;
-		  EEPROM_writeAnything(0, configuration);
+			// es la primera vez que se inicia el sistema
+			  configuration.deflt=0;
+			  configuration.medicion=0;
+			  configuration.minutos_logueo=10;
+			  EEPROM_writeAnything(0, configuration);
 	  }
    else {
     
-    medicion=configuration.medicion;
-    virtualPosition=medicion;
-    Serial.print("medicion en setup:");
-    Serial.println(medicion);
+			medicion=configuration.medicion;
+			virtualPosition=medicion;
+			Serial.print("medicion en setup:");
+			Serial.println(medicion);
     }
     //intervalo de grabacion en la micro sd en milisegundos
     intervalo = configuration.minutos_logueo*60*1000;
 
     seteoReloj();
     initSDcard();
-  
-	
-  pinMode(encoderPinA, INPUT); 
-  pinMode(encoderPinB, INPUT); 
-
- // turn on pullup resistors
-  digitalWrite(encoderPinA, LOW);
-  digitalWrite(encoderPinB, LOW);
-
-
-// encoder pin on interrupt 0 (pin 2)
-  attachInterrupt(digitalPinToInterrupt(encoderPinA), doEncoderA, RISING);
-// encoder pin on interrupt 1 (pin 3)
-  attachInterrupt(digitalPinToInterrupt(encoderPinB), doEncoderB, RISING);
-
-
- 
-
-//pantalla
+	//pantalla
     lcd.begin(16, 2);
     lcd.clear();
 
@@ -138,28 +128,16 @@ void setup() {
 
 void loop() {
 	
-	   rotating = true;  // reset the debouncer
-     String datestring;
-  // muestro la hora y temperatura
-    
-    
-     // if they push the "Save" button, save their configuration
-    if (digitalRead(13) == HIGH)
-        EEPROM_writeAnything(0, configuration);
-    
+    rotating = true;  // reset the debouncer
     
     leerHoraYTemp();
-    leerMedicion();
+    chequearAcumulados();
     mostrarDatos(true);  // Escribimos el valor en pantalla
-    // Listen for buttons 
-    buttonListen();
-    //delay(10000); // ten seconds
+    
 
 }
 
-void resetValues(){
-medicion=0;  
-}
+
 
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
@@ -176,10 +154,34 @@ void leerHoraYTemp(){
  }
 
 
-void leerMedicion(){
+void chequearAcumulados(){
+	//Chequea que no haya cambiado el dia y el mes, si cambió, vuelvo parametros a 0
+	if (configuration.mes_actual!=now.Month()){
+		configuration.acumulado_mes=0;
+		configuration.mes_actual=now.Month();
+	}
 	
-//Ya no hace falta, se hace en mostrar datos, las mediciones se leen en el ISR0
+	if (configuration.dia_actual!=now.Day()){
+		configuration.acumulado_dia=0;
+		configuration.dia_actual=now.Day();
+	}
+	EEPROM_writeAnything(0, configuration);
  
+}
+
+void resetAcumuladoDiario(){
+configuration.acumulado_mes=0;  
+EEPROM_writeAnything(0, configuration);
+}
+
+void resetAcumuladoMensual(){
+configuration.acumulado_dia=0;  
+EEPROM_writeAnything(0, configuration);
+}
+
+void resetAcumuladoTodo(){
+resetAcumuladoDiario();
+resetAcumuladoMensual();
 }
   
 
@@ -192,53 +194,56 @@ void mostrarDatos(boolean serial)
 	   // Serial.print(datestring);
 	    
   //lcd.clear();
-  String s = String(medicion);
-  lcd.setCursor(0,0);
-  lcd.print(datestring);
-  lcd.setCursor(5,1);
-  lcd.print(s + " " + unidades);
+  String s;
   
   //muestro la medicion     
    if (virtualPosition != medicion) {
+		int diff=virtualPosition-medicion;
         medicion = virtualPosition;
+		configuration.acumulado_mes +=diff;
+		configuration.acumulado_dia +=diff;
 		Serial.print("Count:");
         Serial.println(medicion);
-
-    // s = String(medicion);  // Convertimos el número en texto
-    //s.toCharArray(texto, 10);  // Convertimos el texto en un formato compatible
-    // if (serial)
-    //   Serial.println(s + " " + unidades);
-    //lcd.setCursor(5,1);
-    //lcd.print(s + " " + unidades);
-  
-		//cambio el valor de medicion, entonces escribo en la memoria
-		File dataFile = SD.open("datalog.txt", FILE_WRITE);
-    
-		// if the file is available, write to it:
-		if (dataFile) {
-      lcd.setCursor(14,1);
-      lcd.print("  ");
-			dataFile.println(datestring + "," + s + " " + unidades);
-			dataFile.close();
-        }  
-      // if the file isn't open, pop up an error:
-		else {
-			Serial.println("error opening datalog.txt trying to init sd card");
-      initSDcard();
-      }
+        //Grabo en la SD solo si es un nro entero
+		int med_int=(int) medicion;
+		if (medicion=med_int) {
+			//cambio el valor de medicion, entonces escribo en la memoria
+			File dataFile = SD.open("datalog.txt", FILE_WRITE);
+		    
+			// if the file is available, write to it:
+			if (dataFile) {
+				  lcd.setCursor(14,1);
+				  lcd.print("  ");
+				  s = String(medicion);
+				  dataFile.println(datestring + "," + s + " " + unidades);
+				  dataFile.close();
+			}  
+		  // if the file isn't open, pop up an error:
+			else {
+				Serial.println("error opening datalog.txt trying to init sd card");
+				initSDcard();
+			}
+		}
 
 		//escribimos en la eeprom para no perder el valor si el dispositivo se apaga
 		configuration.medicion=medicion;
 		int c=EEPROM_writeAnything(0, configuration);
-   	Serial.print("escribi medicion "); 
-    Serial.print(configuration.medicion) ;
-    Serial.print(" cantidad "); 
-    Serial.print(c); 
-    Serial.println();
+		Serial.print("escribi medicion "); 
+		Serial.print(configuration.medicion) ;
+		Serial.print(" cantidad "); 
+		Serial.print(c); 
+		Serial.println();
   }	
   
-    
-  
+  lcd.setCursor(0,0);
+  lcd.print(datestring);
+  lcd.setCursor(0,1);
+  s = String(round(configuration.acumulado_dia));
+  lcd.print("D:"+ s + " " + unidades);
+  s = String(round(configuration.acumulado_mes));
+  lcd.setCursor(8,1);
+  lcd.print("M:"+ s + " " + unidades);
+
 }
 
 
@@ -342,12 +347,3 @@ void initSDcard(){
      lcd.print("  ");
   }
 
-void buttonListen() {
-
-  if ((lcd.button())==KEYPAD_UP) {
-      lcd.setCursor(0,1);
-     lcd.print("UP");}
-  else {
-     lcd.setCursor(0,1);
-     lcd.print("  ");}
-}
